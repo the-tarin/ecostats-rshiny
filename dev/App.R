4839abc9fa79b3f486dc2a519752a5b1d70400cb
Tarin Eccleston <tarin.eccleston@gmail.com>
2024-01-19 11:55:09 +1300

    observe cell edit

--- dev/App.R
+++ dev/App.R
@@ -11,31 +11,31 @@ library(DT)
 
 ui <- fluidPage(
   titlePanel("Survey of Orangutan Noise Data"),
-  # map #
+  ### map
   leafletOutput("map"),
   ###
   sidebarLayout(
     sidebarPanel(
-      # input files #
-      fileInput("file1", "Upload Mic Coordinate Data in CSV Format",
+      ### file uploads
+      fileInput("fileMic", "Upload Mic Coordinate Data in CSV Format",
                 multiple = TRUE,
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
       
-      fileInput("file2", "Upload Recording Data in CSV Format",
+      fileInput("fileRecordings", "Upload Recording Data in CSV Format",
                 multiple = TRUE,
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
       
-      fileInput("file3", "Upload Gibbon Group Data (Ground Truth) in CSV Format",
+      fileInput("fileGibbons", "Upload Gibbon Group Data (Ground Truth) in CSV Format",
                 multiple = TRUE,
                 accept = c("text/csv",
                            "text/comma-separated-values,text/plain",
                            ".csv")),
       ###
-      # filters #
+      ### filters
       dateInput(inputId = "selected_date", label = "Select Date"),
       
       sliderInput(
@@ -61,7 +61,7 @@ ui <- fluidPage(
       ###
     ),
     
-    # Main panel for displaying outputs #
+    ### Main panel for displaying outputs
     mainPanel(
       plotOutput(outputId = "distPlot"),
       DT::dataTableOutput("recording_table")
@@ -86,7 +86,7 @@ server <- function(input, output, session) {
   
   ### update select date input to earliest date in the recording data
   # todo: complete this to automatically extract the date. Not high priority
-  observeEvent(input$file2, {
+  observeEvent(input$fileRecordings, {
     updateDateInput(
       session,
       "selected_date",
@@ -95,8 +95,8 @@ server <- function(input, output, session) {
     )
   })
   
-  # add mic markers when mic_df is uploaded
-  observeEvent(input$file1, {
+  ### add mic markers when mic_df is uploaded
+  observeEvent(input$fileMic, {
     leafletProxy("map") %>%
       clearMarkers() %>%
       addCircleMarkers(
@@ -119,7 +119,9 @@ server <- function(input, output, session) {
       fitBounds(lng1 = min(mic_df()$X.lon.), lat1 = min(mic_df()$X.lat.),
                 lng2 = max(mic_df()$X.lon.), lat2 = max(mic_df()$X.lat.))
   })
+  ###
   
+  ### main recordings datatable
   output$recording_table <- DT::renderDataTable({
     # not the most ideal solution, but it'll do for now. todo: fix timezone issue
     selected_time_min <- input$selected_time_range[1] + hours(13)
@@ -133,48 +135,60 @@ server <- function(input, output, session) {
         X.measured_call_datetime. <= as.POSIXct(input$selected_time_range[2], format = "%Y-%m-%d %H:%M:%S")
       )
       
-    datatable(recording_df, editable = list(target = 'row', disable = list(columns = c(0, 2, 3, 4, 5, 6, 7))), rownames = FALSE)
+    # datatable(recording_df, editable = list(target = 'row', disable = list(columns = c(0, 2, 3, 4, 5, 6, 7))), rownames = FALSE)
+    datatable(recording_df, editable = list(target = 'cell', disable = list(columns = c(0, 2, 3, 4, 5, 6, 7))), rownames = FALSE)
   })
+  ###
   
   ### selected mic from datatable
   # todo: need to select mic ID from filtered datatable
-  observeEvent(input$recording_table_rows_selected, {
-    selected_rows = input$recording_table_rows_selected
-    selected_mics = recording_df()$X.mic_ID.[selected_rows]
-    print(selected_mics)
-    
-    selected_bearings = recording_df()$X.measured_bearing.[selected_rows]
-    print(selected_bearings)
-    
-    
+  
+  proxy_recording_table = dataTableProxy('recording_table')
+  observeEvent(input$recording_table_cell_edit, {
+    print("hi")
+    # info = input$recording_table_cell_edit
+    # str(info)  # check what info looks like (a data frame of 3 columns)
+    # recording_df <<- editData(recording_df, info)
+    # replaceData(proxy_recording_table, recording_df, resetPaging = FALSE)  # important
+    # # the above steps can be merged into a single editData() call; see examples below
   })
   
-  # # test #
-  # # create a reactiveValues to store the edited data
-  # # edited_data <- reactiveValues(df = NULL)
-  # 
-  # # hi <- reactive({
-  # #   input$recording_table_cell_edit
-  # # })
-  # 
-  # # observer to update edited_data when the table is edited
-  # observeEvent(input$recording_table_cell_edit, {
-  #   # info <- input$recording_table_cell_edit
-  #   # str(info)
-  #   
-  #   print("hi")
-  #   
-  #   # # Check if the data table is not empty and if a cell was edited
-  #   # if (!is.null(info) && !is.null(info$value)) {
-  #   #   edited_data$df <- editData(edited_data$df, info)
-  #   # }
-  # })
-  # ###
   
+  # observeEvent(input$recording_table_rows_selected, {
+  #   selected_rows = input$recording_table_rows_selected
+  #   selected_mics = recording_df()$X.mic_ID.[selected_rows]
+  #   print(selected_mics)
+  #   
+  #   selected_bearings = recording_df()$X.measured_bearing.[selected_rows]
+  #   print(selected_bearings)
+  # })
+  
+  # test #
+  # create a reactiveValues to store the edited data
+  # edited_data <- reactiveValues(df = NULL)
+
+  # hi <- reactive({
+  #   input$recording_table_cell_edit
+  # })
+
+  # observer to update edited_data when the table is edited
+  observeEvent(input$recording_table_cell_edit, {
+    # info <- input$recording_table_cell_edit
+    # str(info)
+
+    print("hi")
+
+    # # Check if the data table is not empty and if a cell was edited
+    # if (!is.null(info) && !is.null(info$value)) {
+    #   edited_data$df <- editData(edited_data$df, info)
+    # }
+  })
+  ###
+
   ### file uploads
   mic_df <- reactive({
-    req(input$file1)
-    mic_df <- read.csv(input$file1$datapath,
+    req(input$fileMic)
+    mic_df <- read.csv(input$fileMic$datapath,
                        header = TRUE,
                        sep = ",",
                        quote = "")
@@ -182,23 +196,21 @@ server <- function(input, output, session) {
   })
   
   recording_df <- reactive({
-    req(input$file2)
-    recording_df <- read.csv(input$file2$datapath,
+    req(input$fileRecordings)
+    recording_df <- read.csv(input$fileRecordings$datapath,
                              header = TRUE,
                              sep = ",",
                              quote = "")
     
-    # recorder by measured datetime of gibbon call
+    # reorder by measured datetime of gibbon call
     recording_df <- recording_df[order(recording_df$X.measured_call_datetime), ]
-    
-    # print(recording_df$X.measured_call_datetime[1])
     
     return(recording_df)
   })
   
   gibbon_df <- reactive({
-    req(input$file3)
-    gibbon_df <- read.csv(input$file3$datapath,
+    req(input$fileGibbons)
+    gibbon_df <- read.csv(input$fileGibbons$datapath,
                           header = TRUE,
                           sep = ",",
                           quote = "")
